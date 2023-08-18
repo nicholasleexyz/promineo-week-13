85afe721ddce73f2938340f59e177b948e65f419
Nicholas Lee <nicholasleexyz@gmail.com>
2023-08-18 16:32:55 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from './LoginForm';
+
+function App() {
+  return (
+    <div className='content'>
+      <LoginForm></LoginForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LoginForm.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function LoginForm() {
+  return (
+    <>
+        <h3>Login</h3>
+        <span>Username: </span>
+        <input type="text" />
+        <span>Password: </span>
+        <input type="text" />
+        <button type="button">asdfdsaf</button>
+    </>
+  );
+}
+
+export default LoginForm;
