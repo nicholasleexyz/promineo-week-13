556210293603905e1486215b32fe994fc6a55ce7
Nicholas Lee <nicholasleexyz@gmail.com>
2023-08-19 05:46:00 -0600

    removed comment

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -31,17 +31,5 @@ function LoginForm() {
     </div>
   );
 }
-  // const [name, setName] = useState("");
 
-  // return (
-  //   <form>
-  //     <label>Enter your name:
-  //       <input
-  //         type="text" 
-  //         value={name}
-  //         onChange={(e) => setName(e.target.value)}
-  //       />
-  //     </label>
-  //   </form>
-  // )
 export default LoginForm;
